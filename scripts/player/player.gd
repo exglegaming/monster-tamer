3801448e061c506032013a2be024c8ba34558efd
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-02 21:52:45 -0500

    updated movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,20 +6,46 @@ const MOVE_LEFT: StringName = "move_left"
 const MOVE_RIGHT: StringName = "move_right"
 const MOVE_UP: StringName = "move_up"
 const MOVE_DOWN: StringName = "move_down"
+const WALK_LEFT: StringName = "walk_left"
+const WALK_RIGHT: StringName = "walk_right"
+const WALK_UP: StringName = "walk_up"
+const WALK_DOWN: StringName = "walk_down"
 
 @export var debug: bool = false
 
 @export_category("Movement Settings")
 @export var move_speed: float = 200.0
-@export var accelaration: float = 5.0
+
+var last_direction: Vector2 = Vector2.ZERO
 
 
-func _physics_process(delta: float) -> void:
-	_move_player(delta)
+@onready var character_animated_sprite: AnimatedSprite2D = %CharacterAnimatedSprite
+
+
+func _physics_process(_delta: float) -> void:
+	_move_player()
 	move_and_slide()
 
 
-func _move_player(delta: float) -> void:
+func _move_player() -> void:
 	var direction: Vector2 = Input.get_vector(MOVE_LEFT, MOVE_RIGHT, MOVE_UP, MOVE_DOWN)
-	var target_velocity: Vector2 = direction * accelaration
-	velocity = velocity.lerp(target_velocity, 1 - exp(-25 * delta))
+	velocity = direction * move_speed
+
+	if direction != Vector2.ZERO:
+		last_direction = direction
+	
+	_update_animation(last_direction)
+
+	
+func _update_animation(dir: Vector2) -> void:
+	if velocity != Vector2.ZERO:
+		if dir.x > 0:
+			character_animated_sprite.play(WALK_RIGHT)
+		elif dir.x < 0:
+			character_animated_sprite.play(WALK_LEFT)
+		elif dir.y < 0:
+			character_animated_sprite.play(WALK_UP)
+		elif dir.y > 0:
+			character_animated_sprite.play(WALK_DOWN)
+	else:
+		character_animated_sprite.stop()
--- scripts/resources/data/monsters/monster.gd
+++ scripts/resources/data/monsters/monster.gd
@@ -2,8 +2,15 @@ class_name Monster
 extends Resource
 
 
+@export var id_number: int
+@export var name: String
+@export_enum("Fire", "Water", "Ground", "Electric", "Flying", "Grass") var type: String
+@export var image: Texture2D
+@export var level: int
+@export var experience_points: float
 @export var health: float
 @export var attack_stat: float
 @export var defense_stat: float
 @export var speed: float
-@export var creature: Creature
+@export var moveset: Array = []
+@export var has_evolution: bool
