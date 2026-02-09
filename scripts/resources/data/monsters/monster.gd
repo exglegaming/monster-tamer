7c28fda11e02c78a47a6cd350c44f3a573fe5dca
ExgleGaming <EaglesFan91x@outlook.com>
2026-02-08 22:32:43 -0500

    change of sprites

--- scripts/resources/data/monsters/monster.gd
+++ scripts/resources/data/monsters/monster.gd
@@ -5,12 +5,13 @@ extends Resource
 @export var id_number: int
 @export var name: String
 @export_enum("Fire", "Water", "Ground", "Electric", "Flying", "Grass") var type: String
-@export var image: Texture2D
-@export var level: int
-@export var experience_points: float
-@export var health: float
-@export var attack_stat: float
-@export var defense_stat: float
-@export var speed: float
-@export var moveset: Array = []
-@export var has_evolution: bool
+@export var back_image: Texture2D
+@export var level: int = 1
+@export var experience_points: float = 0.0
+@export var health: float = 10.0
+@export var attack_stat: float = 1.0
+@export var defense_stat: float = 1.0
+@export var speed: float = 1.0
+@export var moveset: Array = [] # May move this to its own resource but not sure yet
+@export var has_evolution: bool = false
+@export var is_caught: bool = false
